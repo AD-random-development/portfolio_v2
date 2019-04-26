5d693d6b399fafa60d8dd5d6e2d16d681fb245ad
Avinash <avinashdhillor24@gmail.com>
2019-04-26 18:41:48 +0530

    footer and favicon added

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './css/footer.css';
+
+export default () => (
+  <footer className="footer_container">
+    <p className="footer_content">&copy; Created by Avinash Dhillor</p>
+  </footer>
+);
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -9,27 +9,31 @@ import Resume from '../components/Resume';
 import Projects from '../components/Projects';
 import AboutMe from '../components/AboutMe';
 import NotFound from '../components/NotFound';
+import Footer from '../components/Footer';
 
 import '../components/css/common.css';
 
 export default () => (
-  <div className="row">
-    <BrowserRouter>
-      <div className="left_container">
-        <Navbar />
-      </div>
-      <div className="right_scroll">
-        <div className="right_container">
-          <Switch>
-            <Route path="/" component={Landing} exact={true} />
-            <Route path="/skills" component={Skills} exact={true} />
-            <Route path="/resume" component={Resume} exact={true} />
-            <Route path="/projects" component={Projects} exact={true} />
-            <Route path="/aboutme" component={AboutMe} exact={true} />
-            <Route component={NotFound} />
-          </Switch>
+  <div>
+    <div className="row">
+      <BrowserRouter>
+        <div className="left_container">
+          <Navbar />
         </div>
-      </div>
-    </BrowserRouter>
+        <div className="right_scroll">
+          <div className="right_container">
+            <Switch>
+              <Route path="/" component={Landing} exact={true} />
+              <Route path="/skills" component={Skills} exact={true} />
+              <Route path="/resume" component={Resume} exact={true} />
+              <Route path="/projects" component={Projects} exact={true} />
+              <Route path="/aboutme" component={AboutMe} exact={true} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+    <Footer />
   </div>
 );
